010624699a86f9d7fe2fa9c88e89d1de12e2a307
leonguevara <leon.guevara@outlook.com>
2016-02-16 12:32:27 -0600

    Se agregó la funcionalidad de hacer anotaciones en los mapas

--- Memorable Places/ViewController.swift
+++ Memorable Places/ViewController.swift
@@ -9,13 +9,76 @@
 import UIKit
 import MapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
+    
+    var manager : CLLocationManager!
 
     @IBOutlet weak var myMap: MKMapView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        manager = CLLocationManager()
+        manager.delegate = self
+        manager.desiredAccuracy = kCLLocationAccuracyBest
+        manager.requestWhenInUseAuthorization()
+        manager.startUpdatingLocation()
+        
+        let uilpgr = UILongPressGestureRecognizer(target: self, action: "action:")
+        
+        uilpgr.minimumPressDuration = 2.0
+        
+        myMap.addGestureRecognizer(uilpgr)
+    }
+    
+    func action(gestureReconizer: UIGestureRecognizer) {
+        if gestureReconizer.state == UIGestureRecognizerState.Began {
+            let touchPoint = gestureReconizer.locationInView(self.myMap)
+            let newCoordinate = self.myMap.convertPoint(touchPoint, toCoordinateFromView: self.myMap)
+            let location = CLLocation(latitude: newCoordinate.latitude, longitude: newCoordinate.longitude)
+            
+            CLGeocoder().reverseGeocodeLocation(location, completionHandler: { (placemarks, error) -> Void in
+                var title = ""
+                if error == nil {
+                    if let p = placemarks?[0] {
+                        var subThoroughfare : String = ""
+                        var thoroughfare : String = ""
+                        
+                        if p.subThoroughfare != nil {
+                            subThoroughfare = p.subThoroughfare!
+                        }
+                        
+                        if p.thoroughfare != nil {
+                            thoroughfare = p.thoroughfare!
+                        }
+                        title = "\(subThoroughfare) \(thoroughfare)"
+                    }
+                }
+                
+                if title == "" {
+                    title = "Added \(NSDate())"
+                }
+                
+                let annotation = MKPointAnnotation()
+                annotation.coordinate = newCoordinate
+                annotation.title = title
+                self.myMap.addAnnotation(annotation)
+            })
+            
+        }
+    }
+    
+    func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let userLocation : CLLocation = locations[0]
+        let latitude = userLocation.coordinate.latitude
+        let longitude = userLocation.coordinate.longitude
+        let coordinate : CLLocationCoordinate2D = CLLocationCoordinate2DMake(latitude, longitude)
+        let latDelta : CLLocationDegrees = 0.01
+        let lonDelta : CLLocationDegrees = 0.01
+        let span : MKCoordinateSpan = MKCoordinateSpanMake(latDelta, lonDelta)
+        let region : MKCoordinateRegion = MKCoordinateRegionMake(coordinate, span)
+        self.myMap.setRegion(region, animated: true)
     }
 
     override func didReceiveMemoryWarning() {
